0f95d62ce49c7eed37f3f6fbe29fdbfd7c863523
jkimbrunetti <jkim-brunetti27@bentleyschool.org>
2026-04-23 20:00:34 -0700

    Add files via upload

--- CSP_5_03_Traveling_Salesman.py
+++ CSP_5_03_Traveling_Salesman.py
@@ -13,7 +13,7 @@ def getPathDistance(places : list):
 
 def full_TSP(places : list):
     bestRoute = []
-    bestDistance = float('n')
+    bestDistance = float('inf')
     calculations = 0
 
     allPermutations = generatePermutations(places)
@@ -76,43 +76,6 @@ places = [[80,75],[100,520],[530,300],[280,200],[350,150],[700,120],[400,500]]
 
 
 def DrawExample(places):
-    #Draws the TSP showcase to the screen.
-    TSP = full_TSP(places.copy())
-    Hueristic = hueristic_TSP(places.copy())
-    # Initialize Pygame
-    pygame.init()
-    print(TSP)
-    print(Hueristic)
-    # Set up the game window
-    screen = pygame.display.set_mode((800, 800))
-    # Game loop
-    running = True
-    pygame.font.init()
-    font = pygame.font.SysFont(None, 48)
-    text_surface = font.render('Hello, Pygame!', True, (0, 0, 0))
-    text_surface.set_colorkey((0,0,0))
-    text_rect = text_surface.get_rect()
-    text_rect.center = (300, 700)  # Center the text on the screen
-    # Arguments: text string, antialias boolean (True for smooth edges), text color, optional background color
-    text_surface = font.render('Hello, Pygame!', True, (255, 255, 255))  # White text
-    while running:
-        screen.fill((255,255,255))
-        for i in range(len(TSP)-1):
-            pygame.draw.line(screen,(255,0,0),(TSP[i][0],TSP[i][1]),(TSP[i+1][0],TSP[i+1][1]),width = 8)
-        if len(TSP) >=1:pygame.draw.line(screen, (255, 0, 0), (TSP[0][0], TSP[0][1]), (TSP[-1][0], TSP[-1][1]),width =8)
-        for i in range(len(Hueristic) - 1):
-            pygame.draw.line(screen, (0, 0, 255), (Hueristic[i][0], Hueristic[i][1]), (Hueristic[i + 1][0], Hueristic[i + 1][1]), width=4)
-        if len(Hueristic) >=1:pygame.draw.line(screen, (0, 0, 255), (Hueristic[0][0], Hueristic[0][1]), (Hueristic[-1][0], Hueristic[-1][1]), width=4)
-        for spot in places:
-            pygame.draw.circle(screen, (0,0,0),(spot[0],spot[1]), 10)
-        text_surface = font.render('Red is full TSP Blue is Heuristic', True, (0, 0, 0))
-        screen.blit(text_surface, text_rect)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        pygame.display.flip()
-    # Quit Pygame
+    return
 
 DrawExample(places)
-#DrawExample(generate_RandomCoordinates(5))# DO NOT run more than 9 or 10
-pygame.quit()
